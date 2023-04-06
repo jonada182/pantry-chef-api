359b645ddfc49d1fd09429faf3ee8de68925c7a4
jonada182 <jonada182@hotmail.com>
2023-04-06 01:46:50 -0400

    Add groceries data. Add API logger.

--- project/routes.py
+++ project/routes.py
@@ -1,3 +1,4 @@
+import time
 from flask import abort, request, jsonify
 from project import app, openai
 
@@ -13,6 +14,11 @@ def chat():
         if user_message == "":
             abort(400, "Message can't be empty")
 
+        app.logger.info('Fetching response from OpenAI')
+
+        # start request performance counter 
+        start_time = time.perf_counter()
+
         # Generate a response using GPT
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
@@ -21,9 +27,12 @@ def chat():
             ]
         )
 
+        # log request performance
+        request_time = time.perf_counter() - start_time
+        app.logger.info('Got message from OpenAI in {0:.0f}ms' . format(request_time))
+
         # Extract the first generated text from the response
         generated_text = response.choices[0].message.content
-        generated_text = generated_text.replace('\n', '<br/>')
 
         # Return the generated text to the user
         return jsonify({'message': generated_text})
