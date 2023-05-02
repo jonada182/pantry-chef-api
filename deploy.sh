de14d59160590a7e52c0dc52f892b8e81ed542a6
jonada182 <jonada182@hotmail.com>
2023-05-02 02:43:37 -0400

    Fix config issues

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 set -e
-
+aws ecr get-login-password --region ca-central-1 | 
+docker login --username AWS --password-stdin 177004468621.dkr.ecr.ca-central-1.amazonaws.com
 mkdir -p /home/ec2-user/chatbot-api
 cp /tmp/docker-compose.yml /tmp/.env /home/ec2-user/chatbot-api
 cd /home/ec2-user/chatbot-api
